6ac8f50f2c028882fa5e9e018c7bd57ec0c71a93
AlinaNabi <amnabieva@gmail.com>
2021-10-18 01:18:08 +0300

    adding files

---
+++ src/BonusMilesService.java
@@ -0,0 +1,19 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int mile = 2000/20;
+        return mile;
+    }
+}
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int ticketPrice = 2000;
+//        byte bonusMile = 20;
+//        int mile;
+//
+//        mile = ticketPrice / bonusMile;
+//        System.out.println(mile);
+//
+//    }
+//}
